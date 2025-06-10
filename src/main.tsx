9058ccdb029c758e18affc7df0f471a4a00ddae3
Sergio Barreras <sergiobg.isc@gmail.com>
2025-06-09 17:01:54 -0700

    clase: Mi primer componente

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { FirstStepsApp } from "./FirstStepsApp";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <h1>Hola Mundo</h1>
+    <FirstStepsApp />
   </StrictMode>
 );
