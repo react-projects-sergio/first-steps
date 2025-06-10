3e21711cf54e159446d966a386cc73fe9f98bfa9
Sergio Barreras <sergiobg.isc@gmail.com>
2025-06-09 17:30:36 -0700

    clase: Colocar estilos de CSS

--- src/MyAwesomeApp.tsx
+++ src/MyAwesomeApp.tsx
@@ -1,12 +1,20 @@
+import type { CSSProperties } from "react";
+
 const fisrtName = "Sergio";
 const lastName = "Barreras";
 
 const favoriteGames = ["Elden Ring", "Resident Evil"];
-const isActive = false;
+const isActive = true;
 
 const addres = {
   zipCode: "ABC-123",
   country: "Mexico",
+};
+
+const myStyle: CSSProperties = {
+  backgroundColor: "#d3d3d3",
+  borderRadius: isActive ? 10 : 20,
+  padding: 10,
 };
 
 export function MyAwesomeApp() {
@@ -20,7 +28,7 @@ export function MyAwesomeApp() {
 
       <h2>{isActive ? "Activo" : "Inactivo"}</h2>
 
-      <p>{JSON.stringify(addres)}</p>
+      <p style={myStyle}>{JSON.stringify(addres)}</p>
     </>
   );
 }
