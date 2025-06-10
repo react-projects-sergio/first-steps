fa22c57249bd6ae347e02913b0cb8a9aa1edb019
Sergio Barreras <sergiobg.isc@gmail.com>
2025-06-09 17:20:10 -0700

    clase: Impresión de variables

--- src/MyAwesomeApp.tsx
+++ src/MyAwesomeApp.tsx
@@ -1,8 +1,26 @@
+const fisrtName = "Sergio";
+const lastName = "Barreras";
+
+const favoriteGames = ["Elden Ring", "Resident Evil"];
+const isActive = false;
+
+const addres = {
+  zipCode: "ABC-123",
+  country: "Mexico",
+};
+
 export function MyAwesomeApp() {
   return (
     <>
-      <h1>Sergio</h1>
-      <h3>Barreras</h3>
+      <h1>{fisrtName}</h1>
+      <h3>{lastName}</h3>
+
+      <p>{favoriteGames.join(", ")}</p>
+      <p>{2 + 2}</p>
+
+      <h2>{isActive ? "Activo" : "Inactivo"}</h2>
+
+      <p>{JSON.stringify(addres)}</p>
     </>
   );
 }
