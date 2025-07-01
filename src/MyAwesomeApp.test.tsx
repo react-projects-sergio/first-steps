441969f3c2ba483006be181bbc58de551971bfb5
Sergio Barreras <sergiobg.isc@gmail.com>
2025-07-01 12:21:19 -0700

    clase: Buscar elementos en el componente renderizado

--- src/MyAwesomeApp.test.tsx
+++ src/MyAwesomeApp.test.tsx
@@ -3,9 +3,25 @@ import { render, screen } from "@testing-library/react";
 import { MyAwesomeApp } from "./MyAwesomeApp";
 
 describe("MyAwesomeApp", () => {
-  test("should render firstName and lastName", () => {
-    render(<MyAwesomeApp />);
+  test("should render firstName and lastName - render", () => {
+    const { container } = render(<MyAwesomeApp />);
 
+    const h1 = container.querySelector("h1");
+    const h3 = container.querySelector("h3");
+
+    expect(h1?.innerHTML).toContain("Sergio");
+    expect(h3?.innerHTML).toContain("Barreras");
+  });
+
+  test("should render firstName and lastName - screen", () => {
+    render(<MyAwesomeApp />);
     screen.debug();
+
+    const h1 = screen.getByRole("heading", { level: 1 });
+    const h3 = screen.getByRole("heading", { level: 3 });
+
+    console.log(h1.innerHTML);
+    expect(h1?.innerHTML).toContain("Sergio");
+    expect(h3?.innerHTML).toContain("Barreras");
   });
 });
