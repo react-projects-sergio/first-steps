16ee27f26c5334c000338984e1da9d66fa0d9cdd
Sergio Barreras <sergiobg.isc@gmail.com>
2025-07-01 19:41:30 -0700

    clase: Comprobar estilos

--- src/shopping-cart/ItemCounter.test.tsx
+++ src/shopping-cart/ItemCounter.test.tsx
@@ -49,4 +49,24 @@ describe("ItemCounter", () => {
 
     expect(screen.getByText("1")).toBeDefined();
   });
+
+  test("should change to red when count is 1", () => {
+    const name = "Test item";
+
+    render(<ItemCounter name={name} />);
+
+    const itemText = screen.getByText(name);
+
+    expect(itemText.style.color).toBe("red");
+  });
+  
+  test("should change to black when count is greater than 1", () => {
+    const name = "Test item";
+
+    render(<ItemCounter name={name} quantity={2} />);
+
+    const itemText = screen.getByText(name);
+
+    expect(itemText.style.color).toBe("black");
+  });
 });
