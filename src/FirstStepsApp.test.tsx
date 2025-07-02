742535aea7f77ed14b1f9e7950a2bb11e265f7ac
Sergio Barreras <sergiobg.isc@gmail.com>
2025-07-02 07:44:32 -0700

    clase: Componentes fictícios - Mock Components

--- src/FirstStepsApp.test.tsx
+++ src/FirstStepsApp.test.tsx
@@ -1,11 +1,23 @@
-import { render } from "@testing-library/react";
-import { describe, expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, test, vi } from "vitest";
 import { FirstStepsApp } from "./FirstStepsApp";
+
+vi.mock("./shopping-cart/ItemCounter", () => ({
+  ItemCounter: () => <div data-testid="ItemCounter" />,
+}));
 
 describe("FirstStepsApp", () => {
   test("should match snapshot", () => {
     const { container } = render(<FirstStepsApp />);
 
     expect(container).toMatchSnapshot();
+  });
+
+  test("should render the correct number of ItemCounter components", () => {
+    render(<FirstStepsApp />);
+
+    const itemCounters = screen.getAllByTestId("ItemCounter");
+
+    expect(itemCounters.length).toBe(3);
   });
 });
